50aa2a8a2b8e1ed03ce2e377dae6fc1a2b4ed219
Shiro Ushijima <buffalo46@gmail.com>
2022-09-08 13:29:54 -0400

    one more line

--- iterator/aggregate.py
+++ iterator/aggregate.py
@@ -1,5 +1,10 @@
-from turtle import window_width
+import abc
 
+from ..common.model import Interface
+from .iterator import IteratorIF
 
-class AggregateIF():
-    pass
+class AggregateIF(Interface):
+    @abc.abstractclassmethod
+    def iterator(self) -> IteratorIF:
+        pass
+    
