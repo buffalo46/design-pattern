e2ef52026cdc03865c81fe24109af17c5d31e403
Shiro Ushijima <buffalo46@gmail.com>
2022-09-08 09:49:59 -0400

    updated book.py

--- iterator/book.py
+++ iterator/book.py
@@ -1,1 +1,10 @@
+# original: https://saruhei1989.hatenablog.com/entry/2019/04/07/114832
+
+class Book(object):
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
 
